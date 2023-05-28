f91250887d6f2b48b7fa40d3f60fc54e54c0af92
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-05-28 12:47:27 +0900

    set up Mock Service Worker for CSR tests

--- base-concert-venue/jest.setup.js
+++ base-concert-venue/jest.setup.js
@@ -4,3 +4,11 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+import { server } from "@/__tests__/__mocks__/msw/server";
+
+beforeAll(() => server.listen());
+
+beforeEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
